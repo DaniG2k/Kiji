78b9fbb282b3f4abf7d80acfd7ff6c921a08091f
DaniG2k <byakugan.87@gmail.com>
2014-02-11 16:21:22 +0000

    Latest commit

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module Kiji
         false
       end
     end
+    
+    I18n.enforce_available_locales = true
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
