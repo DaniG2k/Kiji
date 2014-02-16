27c4e3a156d9fa0a13bad9a7f6bd69231d4cb2d6
DaniG2k <byakugan.87@gmail.com>
2014-02-16 22:36:11 +0000

    Changed schedule for cron job

--- config/schedule.rb
+++ config/schedule.rb
@@ -31,6 +31,6 @@ screenshots = "#{env} rake scrape:screenshots"
 precompile = "#{env} rake assets:precompile"
 nginx_recache = 'touch /home/dani/Kiji/tmp/restart.txt'
 
-every 5.hours do
+every 6.hours do
   command [chdir, all, zscore, clobber, screenshots, precompile, nginx_recache].join(' && ')
 end
