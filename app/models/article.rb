2edb7fa9041f95f2e4774c8493af72224d547484
DaniG2k <byakugan.87@gmail.com>
2014-03-20 10:33:40 +0000

    Fixed bug in article.rc search method

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
   self.per_page = 50
   
   def self.search(query)
-    query.downcase!
-    where("lower(title) LIKE ? OR lower(source) LIKE ?", "%#{query}%", "%#{query}%")
+    q = query.downcase
+    where("lower(title) LIKE ? OR lower(source) LIKE ?", "%#{q}%", "%#{q}%")
   end
 end
