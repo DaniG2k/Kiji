2b1b827b6a9637700f55e86cdf1e4b4f33ba9986
DaniG2k <byakugan.87@gmail.com>
2014-03-25 16:58:39 +0000

    Working on bouncing summary dropdown

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,13 @@
+/* Place all the behaviors and hooks related to the matching controller here.
+* All this logic will automatically be available in application.js.
+* You can use CoffeeScript in this file: http://coffeescript.org/
+*/
+$(document).ready(function(){
+  $("ol.rounded-list li").click(function(){
+    $(this).find("div.bounce-summary").toggle("slide", {
+    	duration: 1000,
+    	easing: 'easeOutBounce',
+    	direction: 'up'
+    });
+  });
+});
