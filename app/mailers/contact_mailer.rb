f17969f3b07f9ba658a6adceaf47d3263ac592ba
DaniG2k <byakugan.87@gmail.com>
2014-05-22 16:39:25 +0100

    Adding contact mailer

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,9 @@
+class ContactMailer < ActionMailer::Base
+  def receive(email)
+    page = Page.find_by(address: email.to.first)
+    page.emails.create(
+      subject: email.subject,
+      body: email.body
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Kiji::Application.routes.draw do
   get 'month', to: 'home#month'
   get 'week', to: 'home#week'
   get 'yesterday', to: 'home#yesterday'
+  resource :contact, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
