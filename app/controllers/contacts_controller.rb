28ec5135d13830626e43013f913d9b43fb3b2f8f
DaniG2k <byakugan.87@gmail.com>
2014-05-24 14:59:48 +0100

    Added messages partial for the flash

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,9 +6,9 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     if @contact.valid?
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      flash[:notice] = 'Thank you for your message. We will contact you soon!'
     else
-      flash.now[:error] = 'Cannot send message.'
+      flash[:error] = 'Cannot send message.'
       render :new
     end
   end
